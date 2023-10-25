59746522b8fe7d39b9526fce368f428b8e36fcb8
joshuaaa25 <s2200606@student.unklab.ac.id>
2023-10-25 10:46:00 +0800

    Exercise React Router

---
+++ components/About.js
@@ -0,0 +1,11 @@
+function About (){
+    return(
+        <div>
+            <h2>Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website.
+            </h2>
+            Read More the article:
+            <a href="https://en.wikipedia.org/wiki/Front-end_web_development"> https://en.wikipedia.org/wiki/Front-end_web_development 
+            </a>
+        </div>
+    );
+}
---
+++ components/Home.js
@@ -0,0 +1,8 @@
+function Home (){
+    return (
+        <div>
+            <h1>Welcoome to the class front end </h1>
+            <h3>How are you?</h3>
+        </div>
+    );
+}
